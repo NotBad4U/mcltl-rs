669fe3f8d03824c5299cfc32f51bdc471d7d991f
Alessio Coltellacci <lightplay8@gmail.com>
2020-05-01 21:51:53 +0200

    new version

--- src/lib.rs
+++ src/lib.rs
@@ -26,21 +26,21 @@ pub enum LTLExpressionError {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum LTLExpression<'a> {
+pub enum LTLExpression {
     True,
     False,
-    Literal(&'a str),
-    Not(Box<LTLExpression<'a>>),
-    And(Box<LTLExpression<'a>>, Box<LTLExpression<'a>>),
-    Or(Box<LTLExpression<'a>>, Box<LTLExpression<'a>>),
-    G(Box<LTLExpression<'a>>),
-    F(Box<LTLExpression<'a>>),
-    U(Box<LTLExpression<'a>>, Box<LTLExpression<'a>>),
-    R(Box<LTLExpression<'a>>, Box<LTLExpression<'a>>),
-    V(Box<LTLExpression<'a>>, Box<LTLExpression<'a>>),
+    Literal(String),
+    Not(Box<LTLExpression>),
+    And(Box<LTLExpression>, Box<LTLExpression>),
+    Or(Box<LTLExpression>, Box<LTLExpression>),
+    G(Box<LTLExpression>),
+    F(Box<LTLExpression>),
+    U(Box<LTLExpression>, Box<LTLExpression>),
+    R(Box<LTLExpression>, Box<LTLExpression>),
+    V(Box<LTLExpression>, Box<LTLExpression>),
 }
 
-impl<'a> fmt::Display for LTLExpression<'a> {
+impl fmt::Display for LTLExpression {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             LTLExpression::True => write!(f, "T"),
@@ -58,7 +58,7 @@ impl<'a> fmt::Display for LTLExpression<'a> {
     }
 }
 
-impl<'a> LTLExpression<'a> {
+impl LTLExpression {
     pub fn rewrite(&mut self) {
         *self = rewrite(self.clone())
     }
@@ -108,29 +108,38 @@ pub fn rewrite(ltle: LTLExpression) -> LTLExpression {
 */
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct Node {
+pub struct Node {
     id: String,
+    incoming: Vec<Node>,
+    next: Vec<LTLExpression>,
+    oldf: Vec<LTLExpression>,
+    newf: Vec<LTLExpression>,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct Graph<'a> {
-    // The set Nodes stores the set of nodes in the graph
-    Nodes: Set<Node>,
-    //  maps each node of Nodes to a subset of Nodes ∪ {init}, which defines the set of incoming edges
-    Incoming: Map<Node, Set<Node>>,
-    // map each node of Nodes to a set of LTL formulas
-    Now: Map<Node, Set<LTLExpression<'a>>>,
-    // map each node of Nodes to a set of LTL formulas
-    Next: Map<Node, Set<LTLExpression<'a>>>,
-}
-
-impl<'a> Graph<'a> {
-    pub fn new() -> Self {
+impl Node {
+    pub fn new(id: &str) -> Self {
         Self {
-            Nodes: Set::<Node>::new(),
-            Incoming: Map::<Node, Set<Node>>::new(),
-            Now: Map::<Node, Set<LTLExpression>>::new(),
-            Next: Map::<Node, Set<LTLExpression>>::new(),
+            id: id.to_owned(),
+            incoming: vec![],
+            next: vec![],
+            oldf: vec![],
+            newf: vec![],
+        }
+    }
+
+    pub fn new2(
+        id: &str,
+        incoming: Vec<Node>,
+        oldf: Vec<LTLExpression>,
+        newf: Vec<LTLExpression>,
+        next: Vec<LTLExpression>,
+    ) -> Self {
+        Self {
+            id: id.to_owned(),
+            incoming,
+            next,
+            oldf,
+            newf,
         }
     }
 }
@@ -142,189 +151,165 @@ impl<'a> Graph<'a> {
 //     return (Nodes, Now, Incoming)
 // }
 //```
-pub fn create_graph(ltle: LTLExpression) -> Graph {
-    let mut curr = Set::new();
-    curr.insert(ltle);
+pub fn create_graph(f: LTLExpression) -> Vec<Node> {
+    let mut new_begin = vec![f];
 
-    let mut incoming = Set::new();
-    incoming.insert(Node {
-        id: "init".to_owned(),
-    });
+    let init = Node::new("init");
+    let mut incoming = vec![init];
 
-    let mut graph = Graph::new();
+    let mut n = Node::new2(
+        &Uuid::new_v4().to_string(),
+        incoming,
+        vec![],
+        new_begin,
+        vec![],
+    );
+    let mut nodeset = vec![];
+
+    println!("{:#?}", n);
+
     // expand({f}, ∅, ∅, {init} )
-    expand(curr, Set::new(), Set::new(), incoming, &mut graph);
-
-    graph
+    expand(n, nodeset)
 }
 
-// # pseudo-code:
-//```
-// function expand(LTLSet curr, LTLSet old, LTLSet next, NodeSet incoming){
-//     if curr = ∅ then
-//        if ∃q ∈ Nodes: Next(q)=next ∧ Now(q)=old then
-//           Incoming(q)  := Incoming(q) ∪ incoming
-//        else
-//           q  := new_node()
-//           Nodes := Nodes ∪ {q}
-//           Incoming(q)  := incoming
-//           Now(q)  := old
-//           Next(q)  := next
-//           expand(Next(q), ∅, ∅, {q})
-//     else
-//        f ∈ curr
-//        curr  := curr\{f}
-//        old  := old ∪ {f}
-//        match f with
-//         | true, false, p, or ¬p, where  p ∈ AP  →
-//           if f = false ∨ neg(f) ∈ old then
-//              skip
-//           else
-//              expand(curr, old, next, incoming)
-//         | f1 ∧ f2 →
-//           expand(curr ∪ ({f1,f2}\old), old, next, incoming)
-//         | X g →
-//           expand(curr, old, next ∪ {g}, incoming)       
-//         | f1 ∨ f2, f1 U f2, or f1 R f2 →
-//           expand(curr ∪ (curr1(f)\old), old, next ∪ next1(f), incoming)
-//           expand(curr ∪ (curr2(f)\old), old, next, incoming)
-//     return
-// }
-//```
-fn expand<'a>(
-    mut curr: Set<LTLExpression<'a>>,
-    mut old: Set<LTLExpression<'a>>,
-    next: Set<LTLExpression<'a>>,
-    incoming: Set<Node>,
-    graph: &mut Graph<'a>,
-) {
-    //println!("state \n curr={:?}\n old={:?}\n next={:?}\n incoming={:?}", curr, old, next, incoming);
-    if curr.is_empty() {
-        // condition checks if there already exists a state q' with same set of expanded formulas.
-        let mut node = None;
-
-        // if ∃q ∈ Nodes: Next(q) = next ∧ Now(q) = old
-        for q in graph.Nodes.iter() {
-            if graph.Next.get(q).map_or(false, |q| q == &next)
-                && graph.Now.get(q).map_or(false, |q| q == &old)
-            {
-                // counter ownership
-                node = Some(q.clone());
+fn expand<'a>(mut node: Node, mut nodeset: Vec<Node>) -> Vec<Node> {
+    if node.newf.is_empty() {
+        for k in nodeset.iter_mut() {
+            if check_equal_next_and_old(&k, &node) {
+                k.incoming.extend(node.incoming.iter().cloned());
+                return nodeset;
             }
         }
 
-        if let Some(node) = node {
-            // Incoming(q)  := Incoming(q) ∪ incoming
-            let union: Set<Node> = graph.Incoming[&node]
-                .union(&incoming)
-                .map(|n| n.clone())
-                .collect();
-            if let Some(q) = graph.Incoming.get_mut(&node) {
-                *q = union;
-            }
-        } else {
-            // q  := new_node()
-            let q = Node {
-                id: Uuid::new_v4().to_string(),
-            };
-            // Nodes := Nodes ∪ {q}
-            graph.Nodes.insert(q.clone());
-            // Incoming(q)  := incoming
-            *graph.Incoming.entry(q.clone()).or_insert(Set::new()) = incoming;
-            // Now(q)  := old
-            graph.Now.insert(q.clone(), old.clone());
-            //  Next(q)  := next
-            graph.Next.insert(q.clone(), next.clone());
+        nodeset.push(node.clone());
 
-            // expand(Next(q), ∅, ∅, {q})
-            expand(
-                graph.Next.get(&q).unwrap().clone(),
-                set!{},
-                set!{},
-                set!{ q.clone() },
-                graph,
-            );
-        }
-    } else
-    /* f ∈ curr */
-    {
-        let f = curr.clone().iter().next().unwrap().clone();
-        curr.remove(&f); // curr  := curr\{f}
-        old.insert(f.clone()); // old  := old ∪ {f}
+        let incoming = vec![node.clone()];
+        let next = vec![];
+        let newfs = node.next.clone();
+        let oldfs = vec![];
+        let new_node = Node::new2(&Uuid::new_v4().to_string(), incoming, oldfs, newfs, next);
+
+        return expand(new_node, nodeset);
+    } else {
+        let f = node.newf[0].clone();
+        node.newf.remove(0);
 
         match f {
-            LTLExpression::False => (),
-            LTLExpression::Not(_) if old.contains(&LTLExpression::Not(Box::new(f.clone()))) => (), // neg(f) ∈ old
-            LTLExpression::True | LTLExpression::Literal(_) | LTLExpression::Not(_) => {
-                expand(curr, old, next, incoming, graph);
+            LTLExpression::False => {
+                return nodeset;
             }
-            LTLExpression::And(e1, e2) => {
-                // {f1, f2}
-                let e1 = e1.as_ref().clone();
-                let e2 = e2.as_ref().clone();
-                let mut set_e1_e2 = Set::new();
-                set_e1_e2.insert(e1);
-                set_e1_e2.insert(e2);
-
-                // {f1, f2}\old
-                let set_e1_e2_intersect_old: Set<_> =
-                    set_e1_e2.intersection(&old).map(|e| e.clone()).collect();
-
-                // curr U ({f1, f2}\old)
-                let new_curr = curr
-                    .union(&set_e1_e2_intersect_old)
-                    .map(|c| c.clone())
-                    .collect();
-
-                // expand(curr ∪ ({f1,f2}\old), old, next, incoming)
-                expand(new_curr, old, next, incoming, graph);
+            LTLExpression::Not(_) if node.oldf.contains(&f) => {
+                return nodeset;
             }
-            LTLExpression::Or(_, _) | LTLExpression::U(_, _) | LTLExpression::R(_, _) => {
-                let curr1 = curr1(f.clone());
-                let intersect = curr1.intersection(&old).map(|e| e.clone()).collect(); // curr1(f)\old
-                let union1 = curr.union(&intersect).map(|e| e.clone()).collect(); // curr ∪ (curr1(f)\old)
-
-                let next_union_next1_of_f = next.union(&next1(f.clone())).map(|e| e.clone()).collect();
-
-                // expand(curr ∪ (curr1(f)\old), old, next ∪ next1(f), incoming)
-                expand(union1, old.clone(), next_union_next1_of_f, incoming.clone(), graph);
-
-                let curr2 = curr2(f);
-                let intersect = curr2.intersection(&old).map(|e| e.clone()).collect(); // curr2(f)\old
-                let union1 = curr.union(&intersect).map(|e| e.clone()).collect(); // curr U curr2(f)\old
-                // expand(curr ∪ (curr2(f)\old), old, next, incoming)
-                expand(union1, old, next, incoming, graph);
+            LTLExpression::Literal(_) | LTLExpression::True | LTLExpression::Not(_) => {
+                node.oldf.push(f);
+                return expand(node, nodeset);
             }
-            _ => panic!("Should use rewrite"),
+            LTLExpression::And(ref f1, ref f2) => {
+                let f = f.clone();
+                node.oldf.push(f);
+                node.newf.push(f1.as_ref().clone());
+                node.newf.push(f2.as_ref().clone());
+                return expand(node, nodeset);
+            }
+            LTLExpression::U(ref f1, ref f2)
+            | LTLExpression::Or(ref f1, ref f2)
+            | LTLExpression::R(ref f1, ref f2) => {
+                let incoming1 = node.incoming.clone();
+                let mut next1 = node.next.clone();
+                next1.push(f.clone());
+                let mut newfs1 = node.newf.clone();
+
+                let new1 = new1(f.clone());
+                for t in new1.into_iter().filter(|f| !node.oldf.contains(f)) {
+                    newfs1.push(t);
+                }
+                let mut oldfs1 = node.oldf.clone();
+                oldfs1.push(f.clone());
+
+                let node1 = Node::new2(&Uuid::new_v4().to_string(), incoming1, oldfs1, newfs1, next1);
+
+
+                let incoming2 = node.incoming.clone();
+                let mut next2 = node.next.clone();
+                let mut newfs2 = node.newf.clone();
+
+                let new2 = new2(f.clone());
+                for t in new2.into_iter().filter(|f| !node.oldf.contains(f)) {
+                    newfs2.push(t);
+                }
+                let mut oldfs2 = node.oldf.clone();
+                oldfs2.push(f.clone());
+
+                let node2 = Node::new2(&Uuid::new_v4().to_string(), incoming2, oldfs2, newfs2, next2);
+
+                return expand(node2, expand(node1, nodeset))
+            }
+            _ => unimplemented!(),
         }
+
+        return nodeset;
     }
+
+    vec![]
 }
 
-fn curr1(ltle: LTLExpression) -> Set<LTLExpression> {
+fn new1(ltle: LTLExpression) -> Set<LTLExpression> {
     match ltle {
-        LTLExpression::U(f1, _) => set!{ f1.as_ref().clone() },
-        LTLExpression::R(_, f2) => set!{ f2.as_ref().clone() },
-        LTLExpression::Or(_, f2) => set!{ f2.as_ref().clone() },
-        _ => set!{},
+        LTLExpression::U(f1, _) => set! { f1.as_ref().clone() },
+        LTLExpression::R(_, f2) => set! { f2.as_ref().clone() },
+        LTLExpression::Or(_, f2) => set! { f2.as_ref().clone() },
+        _ => set! {},
     }
 }
 
-fn curr2(ltle: LTLExpression) -> Set<LTLExpression> {
+fn new2(ltle: LTLExpression) -> Set<LTLExpression> {
     match ltle {
-        LTLExpression::U(_, f2) => set!{ f2.as_ref().clone() },
-        LTLExpression::R(f1, f2) => set!{ f1.as_ref().clone() , f2.as_ref().clone() },
-        LTLExpression::Or(f1, _) => set!{ f1.as_ref().clone() },
-        _ => set!{},
+        LTLExpression::U(_, f2) => set! { f2.as_ref().clone() },
+        LTLExpression::R(f1, f2) => set! { f1.as_ref().clone() , f2.as_ref().clone() },
+        LTLExpression::Or(f1, _) => set! { f1.as_ref().clone() },
+        _ => set! {},
     }
 }
-
 
 fn next1(ltle: LTLExpression) -> Set<LTLExpression> {
     match ltle {
-        LTLExpression::U(f1, f2) => set!{ LTLExpression::U(f1, f2) },
-        LTLExpression::R(f1, f2) => set!{ LTLExpression::R(f1, f2) },
-        _ => set!{},
+        LTLExpression::U(f1, f2) => set! { LTLExpression::U(f1, f2) },
+        LTLExpression::R(f1, f2) => set! { LTLExpression::R(f1, f2) },
+        _ => set! {},
     }
+}
+
+fn check_equal_next_and_old(k: &Node, n: &Node) -> bool {
+    if k.id == "init" || n.id == "init" {
+        return false;
+    }
+
+    for f in k.next.iter() {
+        if !n.next.contains(f) {
+            return false;
+        }
+    }
+
+    for f in n.next.iter() {
+        if !k.next.contains(f) {
+            return false;
+        }
+    }
+
+    for f in k.oldf.iter() {
+        if !n.oldf.contains(f) {
+            return false;
+        }
+    }
+
+    for f in n.oldf.iter() {
+        if !k.oldf.contains(f) {
+            return false;
+        }
+    }
+
+    true
 }
 
 #[cfg(test)]
@@ -334,32 +319,15 @@ mod tests {
 
     #[test]
     fn display_ltl_expression() {
-        let mut expr = LTLExpression::G(Box::new(LTLExpression::U(
-            Box::new(LTLExpression::Literal("p")),
-            Box::new(LTLExpression::Not(Box::new(LTLExpression::Literal("q")))),
-        )));
-
-        expr.rewrite();
-
-        println!("{}", expr);
-
-        //println!("{}", rewrite(expr));
-    }
-
-    #[test]
-    fn translate_ltl_into_graph() {
         let mut expr = LTLExpression::U(
-            Box::new(LTLExpression::Literal("p")),
-            Box::new(LTLExpression::Literal("q")),
+            Box::new(LTLExpression::Literal("p".to_owned())),
+            Box::new(LTLExpression::Literal("q".to_owned())),
         );
 
         expr.rewrite();
 
-        let graph = create_graph(expr);
+        let nodes = create_graph(expr);
 
-        println!("{:#?}", graph.Nodes);
-        println!("{:#?}", graph.Incoming);
-        println!("{:#?}", graph.Now);
-        println!("{:#?}", graph.Next);
+        println!("{:?}", nodes[0]);
     }
 }
