95def44060478de28a2f62a2dd13a9e78b897469
Alessio Coltellacci <lightplay8@gmail.com>
2020-05-09 22:00:22 +0200

    feature(test): add macro to help to construct generalized Büchi automaton

--- src/utils.rs
+++ src/utils.rs
@@ -31,3 +31,37 @@ macro_rules! buchi{
         __graph
     }};
 }
+
+#[macro_export]
+macro_rules! gbuchi{
+    (
+        $(
+            $src: ident
+                $([$ltl:expr] => $dest: ident)*
+        )*
+        ===
+        init = [$( $init:ident ),*]
+        $(accepting = [$( $accepting_states:expr ),*])*
+    ) => {{
+        let mut __graph = GeneralBuchi::new();
+        $(
+            let mut $src = BuchiNode::new(stringify!($src).to_string());
+            $(
+                $src.adj.push(
+                    BuchiNode {
+                        id: stringify!($dest).into(),
+                        labels: vec![$ltl],
+                        adj: vec![],
+                    }
+                );
+            )*
+
+            __graph.adj_list.push($src.clone());
+        )*
+
+        $(__graph.init_states.push($init.clone());)*
+        $($(__graph.accepting_states.push($accepting_states.clone());)*)*
+
+        __graph
+    }};
+}
