b77116f8109735e99e21b2237f46e2f181d76c31
Alessio Coltellacci <lightplay8@gmail.com>
2020-05-14 21:23:06 +0200

    test(integration): add the first integrations tests

--- tests/verifier.rs
+++ tests/verifier.rs
@@ -15,3 +15,27 @@ fn it_should_hold_simple_until() {
     let res = verifier::verify("./tests/test-data/program2.kripke", property);
     assert!(res.is_ok());
 }
+
+#[test]
+fn it_should_not_hold_simple_until3() {
+    let property = "a U b";
+
+    let res = verifier::verify("./tests/test-data/program3.kripke", property);
+    assert!(res.is_err());
+}
+
+#[test]
+fn it_should_not_hold_simple_until4() {
+    let property = "a U (b or c)";
+
+    let res = verifier::verify("./tests/test-data/program3.kripke", property);
+    assert!(res.is_err());
+}
+
+#[test]
+fn it_should_not_hold_simple_until5() {
+    let property = "(a U (b U c))";
+
+    let res = verifier::verify("./tests/test-data/program4.kripke", property);
+    assert!(res.is_ok());
+}
